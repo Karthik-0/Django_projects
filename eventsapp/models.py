8f8971b4c749dbaa2d4f308de90f7bb580295912
karthikraja34 <rajakarthik131@gmail.com>
2018-07-31 15:36:27 +0530

    Remove unused app

--- eventsapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from model_utils.models import TimeStampedModel
-from taggit.managers import TaggableManager
-
-
-class Event(TimeStampedModel):
-    title = models.CharField(max_length=50)
-    place = models.ForeignKey("placesapp.Place", on_delete=models.CASCADE)
-    tags = TaggableManager()
