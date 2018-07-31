b5ac108edbf8cc5f45de403daedd9cbd4f0b2204
karthikraja34 <rajakarthik131@gmail.com>
2018-07-31 18:58:04 +0530

    Create DetailView for moviesapp

--- moviesapp/views.py
+++ moviesapp/views.py
@@ -5,3 +5,10 @@ from .models import Movie
 class MovieListView(generic.ListView):
     model = Movie
     template_name = "movies/index.html"
+    context_object_name = 'movies'
+
+
+class MovieDetailView(generic.DetailView):
+    model = Movie
+    template_name = "movies/detail.html"
+    context_object_name = 'movie'
