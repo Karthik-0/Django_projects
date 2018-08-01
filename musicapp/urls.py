23d9d800c8a356c98fb8510c002bc6d72c4631d3
karthikraja34 <rajakarthik131@gmail.com>
2018-08-01 19:32:06 +0530

    Add listview for tracks

--- musicapp/urls.py
+++ musicapp/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'music'
 
 urlpatterns = [
-    path('', views.MusicList.as_view(), name="index"),
+    path('', views.AlbumList.as_view(), name="index"),
+    path('detail/<slug>', views.AlbumDetail.as_view(), name="detail"),
+    path('songs/', views.SongList.as_view(), name="song_index"),
+    path('song/detail/<slug>', views.SongDetail.as_view(), name="song_detail"),
 ]
--- musicapp/views.py
+++ musicapp/views.py
@@ -1,8 +1,26 @@
 from django.views import generic
-from .models import Album
+from .models import Album, Track
 
 
-class MusicList(generic.ListView):
+class AlbumList(generic.ListView):
     template_name = "music/index.html"
     model = Album
     context_object_name = 'albums'
+
+
+class AlbumDetail(generic.DetailView):
+    template_name = "music/detail.html"
+    model = Album
+    context_object_name = 'album'
+
+
+class SongList(generic.ListView):
+    template_name = "music/songlist.html"
+    model = Track
+    context_object_name = 'tracks'
+
+
+class SongDetail(generic.DetailView):
+    template_name = "music/songdetail.html"
+    model = Track
+    context_object_name = 'track'
