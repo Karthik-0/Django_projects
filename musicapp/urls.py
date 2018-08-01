d63e74886cacad6434bb2590984052d9a7f22748
karthikraja34 <rajakarthik131@gmail.com>
2018-08-01 18:46:47 +0530

    Add Album listview for musicapp

--- musicapp/urls.py
+++ musicapp/urls.py
@@ -1,5 +1,8 @@
-# from django.urls import path, include
+from django.urls import path
+from . import views
+
+app_name = 'music'
 
 urlpatterns = [
-
+    path('', views.MusicList.as_view(), name="index"),
 ]
