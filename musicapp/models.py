0c0197e32a0604bd5a931b283ef52ce8a4377ace
karthikraja34 <rajakarthik131@gmail.com>
2018-08-01 17:45:42 +0530

    Add models to musicapp

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+gender = (
+    ('male', 'Male'),
+    ('female', 'Female'),
+    ('other', 'Other'),
+)
+
+
+class SlugMixin(models.Model):
+
+    slug = models.SlugField(blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+        super(SlugMixin, self).save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
+
+
+class Track(SlugMixin):
+    name = models.CharField(("Track Name"), max_length=100)
+    band = models.ForeignKey("Band", on_delete=models.CASCADE,
+                             blank=True, null=True, related_name="tracks")
+    album = models.ForeignKey("Album", on_delete=models.CASCADE,
+                              blank=True, null=True, related_name="tracks")
+    song = models.FileField(upload_to="songs/", max_length=150)
+    genre = models.ManyToManyField("Genre")
+
+
+class Album(SlugMixin):
+    title = models.CharField(("Title"), max_length=50)
+    prefix = models.CharField(max_length=50)
+    asin = models.CharField(("ASIN"), max_length=10)
+    band = models.ForeignKey("Band", on_delete=models.CASCADE,
+                             blank=True, null=True, related_name="albums")
+    label = models.ForeignKey("Label", on_delete=models.CASCADE,
+                              blank=True, null=True, related_name="albums")
+    cover_image = models.FileField(upload_to="images/", max_length=100)
+    release_date = models.DateField(auto_now=False, auto_now_add=False)
+
+
+class Genre(SlugMixin):
+    name = models.CharField(max_length=50)
+
+
+class Label(models.Model):
+    name = models.CharField(max_length=150)
+    head_quarters = models.CharField(max_length=50)
+    website = models.URLField(max_length=200)
+
+
+class Musician(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    gender = models.CharField(
+        choices=gender,
+        default='male',
+        max_length=6
+    )
+    band = models.ForeignKey("Band", on_delete=models.CASCADE,
+                             related_name="musicians")
+    instrument = models.CharField(max_length=50)
+
+
+class Band(models.Model):
+    genre = models.ManyToManyField("Genre")
+    website = models.URLField(max_length=200)
