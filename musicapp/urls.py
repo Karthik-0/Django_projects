ec12f33b3759432cc7061e0acdb2df02d8e4e742
karthikraja34 <rajakarthik131@gmail.com>
2018-08-02 09:51:07 +0530

    Complete list view and detail view for track

--- musicapp/urls.py
+++ musicapp/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'music'
 
 urlpatterns = [
-    path('', views.AlbumList.as_view(), name="index"),
-    path('detail/<slug>', views.AlbumDetail.as_view(), name="detail"),
-    path('songs/', views.SongList.as_view(), name="song_index"),
+    path('', views.AlbumList.as_view(), name="album_list"),
+    path('detail/<slug>', views.AlbumDetail.as_view(), name="album_detail"),
+    path('songs/', views.SongList.as_view(), name="song_list"),
     path('song/detail/<slug>', views.SongDetail.as_view(), name="song_detail"),
 ]
