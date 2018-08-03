dda4d41a5b2cffa1e761da315272432efbac6666
karthikraja34 <rajakarthik131@gmail.com>
2018-08-03 10:17:58 +0530

    Add field to model

--- forumapp/models.py
+++ forumapp/models.py
@@ -54,3 +54,7 @@ class Comment(TimeStampedModel):
     created = models.DateField(auto_now_add=True)
     post = models.ForeignKey("Post", on_delete=models.CASCADE,
                              related_name="comments")
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content
