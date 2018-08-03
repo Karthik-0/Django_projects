46c27147497fa7f509c9f23078700f584c2f5c93
karthikraja34 <rajakarthik131@gmail.com>
2018-08-03 10:18:12 +0530

    Register model in admin

--- forumapp/admin.py
+++ forumapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Thread, Post
+from .models import User, Thread, Post, Comment
 
 
 admin.site.register(User)
 admin.site.register(Thread)
 admin.site.register(Post)
+admin.site.register(Comment)
